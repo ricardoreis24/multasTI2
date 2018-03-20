de117803001bf388cdbf6776eed9f0311ea37145
Ricardo Reis <ricardo.galinha@hotmail.com>
2018-03-20 13:03:52 +0000

    Base de Dados operacional

--- Multas/Multas/Models/MultasDB.cs
+++ Multas/Multas/Models/MultasDB.cs
@@ -6,15 +6,22 @@ using System.Web;
 
 namespace Multas.Models
 {   //class especial que vai extender de uma outra que a framework entity adicionou
-    //representa uma base de dados
+    //representa uma base de dados, descreve uma base de dados
     public class MultasDB : DbContext
-    {
+    {   
+        //construtor q indica qual a base de dados a utilizar
+        public MultasDB() : base("name=MultasDBConnectionString")
+        {
+
+        }
         //descrever nomes das tabelas na base de dados
         //virtual so carrega na memoria se necessita
         public virtual DbSet<Multas> Multas { get; set; } //cria tabela das multas
         public virtual DbSet<Condutores> Condutores { get; set; } //tabela dos condutores
         public virtual DbSet<Agentes> Agentes { get; set; } //tabela dos agentes
         public virtual DbSet<Viaturas> Viaturas { get; set; } //tabela dos agentes
-
+       //não esquecer criar referencia do motor no web.config
+       //é necessario referencia ao motor DB
+       //Enable-Migrations -EnableAutomaticMigrations -- usar na consola do NuGet para compilar
     }
 }
