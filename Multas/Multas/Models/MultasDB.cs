bcb8603a94ad063ae0eb1a4b05999360694de3a5
Ricardo Reis <ricardo.galinha@hotmail.com>
2018-03-20 12:28:02 +0000

    Criação das tabelas BD

--- Multas/Multas/Models/MultasDB.cs
+++ Multas/Multas/Models/MultasDB.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace Multas.Models
-{
-    public class MultasDB
+{   //class especial que vai extender de uma outra que a framework entity adicionou
+    //representa uma base de dados
+    public class MultasDB : DbContext
     {
-
-
+        //descrever nomes das tabelas na base de dados
+        //virtual so carrega na memoria se necessita
+        public virtual DbSet<Multas> Multas { get; set; } //cria tabela das multas
+        public virtual DbSet<Condutores> Condutores { get; set; } //tabela dos condutores
+        public virtual DbSet<Agentes> Agentes { get; set; } //tabela dos agentes
+        public virtual DbSet<Viaturas> Viaturas { get; set; } //tabela dos agentes
 
     }
 }
