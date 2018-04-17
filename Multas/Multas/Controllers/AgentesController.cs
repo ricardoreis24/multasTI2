8f0eb0dd740bba21cb687efaab54792775c10b0c
Ricardo Reis <ricardo.galinha@hotmail.com>
2018-04-17 11:11:02 +0100

    Adição de comentarios e metodos para fotografias

--- Multas/Multas/Controllers/AgentesController.cs
+++ Multas/Multas/Controllers/AgentesController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+﻿using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -12,15 +10,17 @@ namespace Multas.Controllers
 {
     public class AgentesController : Controller
     {
-
-        private MultasDB db = new MultasDB();
+        private readonly MultasDB db = new MultasDB();
 
         // GET: Agentes
         public ActionResult Index()
         {
             //db.Agentes.ToList() em SQL SELECT * FROM Agentes;
-            //enviar para a view uma lista com todos os agentes da base de dados
-            return View(db.Agentes.ToList());
+            //enviar para a view uma lista com todos os agente da base de dados
+            //em sql: select * from agentes order by nome;
+            var listaDeAgentes = db.Agentes.ToList().OrderBy(a => a.Nome);
+
+            return View(listaDeAgentes);
         }
 
         // GET: Agentes/Details/5
@@ -28,20 +28,14 @@ namespace Multas.Controllers
         public ActionResult Details(int? id)
         {
             //proteção para o caso de não ter sido fornecido um id
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+            if (id == null) return RedirectToAction("index");
             //procura na BD, o agente cujo ID foi fornecido
-            
-            Agentes agentes = db.Agentes.Find(id);
+
+            var agente = db.Agentes.Find(id);
             //proteção para o caso de não ter sido encontrado qualquer agente
             //que tenha o ID fornecido
-            if (agentes == null)
-            {
-                return HttpNotFound();
-            }
-            return View(agentes);
+            if (agente == null) return RedirectToAction("index");
+            return View(agente);
         }
 
         // GET: Agentes/Create
@@ -56,41 +50,80 @@ namespace Multas.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Nome,Esquadra,Fotografia")] Agentes agentes)
+        public ActionResult Create([Bind(Include = "ID,Nome,Esquadra")] Agentes agente, HttpPostedFileBase uploadFoto)
         {
+            //escrever um ficheiro com a fotografia no disco na pasta 'imagens'          
+            //especificar o id do agente
+            //formatar o tamanho da imagem
+            var idNovoAgente = db.Agentes.Max(a => a.ID) + 1;
+            //guardar id do novo agente
+            agente.ID = idNovoAgente;
+            //especificar (escolher) o nome do ficheiro
+            var nomeImagem = "Agente_" + idNovoAgente + ".jpg";
+
+            //var auxiliar
+            var path = "";
+
+            //validar se imagem foi fornecida
+            if (uploadFoto != null)
+            {
+                //o ficheiro foi fornecido
+                //criar o caminho
+                path = Path.Combine(Server.MapPath("~/imagens"), nomeImagem);
+
+                //guardar o nome do ficheiro na bd
+                agente.Fotografia = nomeImagem;
+            }
+            else
+            {
+                //não foi fornecido qualquer ficheiro
+                ModelState.AddModelError("", "Não foi forncecida uma imagem...");
+                //devolver o controlo da view
+                return View(agente);
+            }
+
             //escreve os dados de um novo agente na DB
             //ModelState.IsValid confronta os dados fornecidos na view
             if (ModelState.IsValid)
             {
-                db.Agentes.Add(agentes);
+                db.Agentes.Add(agente);
                 db.SaveChanges();
+                uploadFoto.SaveAs(path);
                 return RedirectToAction("Index");
             }
 
-            return View(agentes);
+            return View(agente);
         }
 
         // GET: Agentes/Edit/5
         public ActionResult Edit(int? id)
         {
+            //proteção para o caso de não ter sido fornecido um id
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return RedirectToAction("index");
             }
-            Agentes agentes = db.Agentes.Find(id);
-            if (agentes == null)
-            {
-                return HttpNotFound();
-            }
-            return View(agentes);
+            //procura na BD, o agente cujo ID foi fornecido
+
+            var agente = db.Agentes.Find(id);
+            //proteção para o caso de não ter sido encontrado qualquer agente
+            //que tenha o ID fornecido
+            if (agente == null) return RedirectToAction("index");
+            return View(agente);
         }
 
         // POST: Agentes/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="agentes"></param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Nome,Esquadra,Fotografia")] Agentes agentes)
+        public ActionResult Edit([Bind(Include = "ID,Nome,Esquadra,Fotografia")]
+            Agentes agentes)
         {
             if (ModelState.IsValid)
             {
@@ -101,30 +134,38 @@ namespace Multas.Controllers
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
+
             return View(agentes);
         }
 
         // GET: Agentes/Delete/5
+        /// <summary>
+        /// apresenta na view os dados de um agente com vista à sua eventual eliminação
+        /// </summary>
+        /// <param name="id"> indentificador do agente </param>
+        /// <returns></returns>
+
         public ActionResult Delete(int? id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Agentes agentes = db.Agentes.Find(id);
+            if (id == null) return RedirectToAction("Index");
+            //pesquisar pelo agente cujo ID foi fornecido
+            var agentes = db.Agentes.Find(id);
+            //verificar se o agente foi encontrado
             if (agentes == null)
             {
-                return HttpNotFound();
+                return RedirectToAction("Index");
             }
+
             return View(agentes);
         }
 
         // POST: Agentes/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
+        [ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Agentes agentes = db.Agentes.Find(id);
+            var agentes = db.Agentes.Find(id);
             //remove o agente
             db.Agentes.Remove(agentes);
             //commit
@@ -134,10 +175,7 @@ namespace Multas.Controllers
 
         protected override void Dispose(bool disposing)
         {
-            if (disposing)
-            {
-                db.Dispose();
-            }
+            if (disposing) db.Dispose();
             base.Dispose(disposing);
         }
     }
--- Multas/Multas/Models/Agentes.cs
+++ Multas/Multas/Models/Agentes.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -15,15 +16,16 @@ namespace Multas.Models
         }
         //tudo o que tem [] são complementos
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         //descrição dos atributos
         public int ID { get; set; }
 
         //não esquecer o rebuild
         [Required(ErrorMessage = "Tens de preencher o campo {0} , burro.")]
-        [RegularExpression("[A-ZÍÉÂÁ][a-z]+[a-záéíóúàèìòùâêîôûäëïöüãõç]+ [A-ZÍÉÂÁ][a-z]+[a-záéíóúàèìòùâêîôûäëïöüãõç]+(( | '|-| dos | da | de | e | d')[A-ZÍÉÂÁ][a-z] +[a-záéíóúàèìòùâêîôûäëïöüãõç]+[A-ZÍÉÂÁ][a-z]+[a-záéíóúàèìòùâêîôûäëïöüãõç]+){1, 3}", ErrorMessage = "O {0} apenas pode conter letras e espaços em branco. Cada palavra começa em Maiúscula seguida de minúscula")]
+        //[RegularExpression("[A-ZÍÉÂÁ][a-z]+[a-záéíóúàèìòùâêîôûäëïöüãõç]+ [A-ZÍÉÂÁ][a-z]+[a-záéíóúàèìòùâêîôûäëïöüãõç]+(( | '|-| dos | da | de | e | d')[A-ZÍÉÂÁ][a-z] +[a-záéíóúàèìòùâêîôûäëïöüãõç]+[A-ZÍÉÂÁ][a-z]+[a-záéíóúàèìòùâêîôûäëïöüãõç]+){1, 3}", ErrorMessage = "O {0} apenas pode conter letras e espaços em branco. Cada palavra começa em Maiúscula seguida de minúscula")]
 
         public string Nome { get; set; }
-        [RegularExpression("[A-ZÍÉÂÁ]*[a-záéíóúàèìòùâêîôûäëïöüãõç ]*", ErrorMessage = "A {0} não é válida")]
+        //[RegularExpression("[A-ZÍÉÂÁ]*[a-záéíóúàèìòùâêîôûäëïöüãõç ]*", ErrorMessage = "A {0} não é válida")]
         [Required(ErrorMessage = "Tens de preencher o campo {0} , burro.")]
         public string Esquadra { get; set; }
         public string Fotografia { get; set; }
